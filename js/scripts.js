fd4c83c956cfd9f696b74c19018b74a03be6123a
Alicja Chruszczyk <alicja.chruszczyk@gmail.com>
2018-02-28 23:23:08 +0100

    Add js scripts

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,20 @@
 
 // this is another one
 
+var triangleBase = prompt('Triangle base:',''),
+	triangleHeight = prompt('Triangle height:',''),
+	triangleArea;
+
+triangleArea = triangleBase * triangleHeight / 2;
+
+alert('Your triangle area is ' + triangleArea);
+
+/*
+var a = 20,
+	h = 13,
+	triangleArea;
+
+triangleArea = a * h / 2;
+
+console.log('Triangle field with base a: ', a, 'and height h: ', h, ' is equal to: ', triangleArea); 
+*/
